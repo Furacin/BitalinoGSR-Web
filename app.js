3ffc0aa2739d82580f20f06ee782071ac2bc1c08
Furacin <manzanile@gmail.com>
2017-06-09 01:32:43 +0200

    Añadido index y javascript,
    Ejemplo en el que extraemos de firebase valores y los pintamos en una gráfica

---
+++ app.js
@@ -0,0 +1,59 @@
+(function() {
+
+// Initialize Firebase
+    const config = {
+        apiKey: "AIzaSyA-f9NMWCpap1SmlKs9nVWkTX9LJNAOSC0",
+        authDomain: "mbandgsr.firebaseapp.com",
+        databaseURL: "https://mbandgsr.firebaseio.com",
+        projectId: "mbandgsr",
+        storageBucket: "mbandgsr.appspot.com",
+        messagingSenderId: "457411073799"
+    };
+    firebase.initializeApp(config);
+    
+//    var holaMundo = document.getElementById('holaMundo');
+//          var dbRef = firebase.database().ref().child('users').child('fe06b1b1-4add-41fe-b1c7-2bcc907e42d3').child('email');
+//          dbRef.on('value', snap => holaMundo.innerText = snap.val());   
+    
+    // Obtener elementos
+    const ulList = document.getElementById('lista');
+    
+    // Crear referencias
+    const dbRefObject = firebase.database().ref().child('users');
+    const dbRefList = dbRefObject.child('fe06b1b1-4add-41fe-b1c7-2bcc907e42d3').child('Experiencias').child('201706082356').child('Datos Graficas').child('GSR');
+//    const dbRefList = dbRefObject.child('habilidades');
+    
+    var valores_fc = [];
+    
+    // Sincronizar objectos con la web
+    dbRefList.on('value', snap => {
+        var data = snap.val();  
+        
+//        valores_fc.push({title: data.title, content: data.content});
+        
+        const li = document.createElement('li'); 
+        li.innerText = data;
+        ulList.appendChild(li);
+        
+        var ctx = document.getElementById('myChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+              type: 'line',
+              data: {
+                labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+                datasets: [{
+                  label: 'gsr',
+                  data: data,
+                  backgroundColor: "rgba(153,255,51,0.4)"
+                }]
+              }
+            });
+    });
+    
+//    console.log(valores_fc);
+//    console.log(valores_fc[0]);
+    
+//    for (var i=0; i< valores_fc.length; i++) {
+//        alert(valores_fc[i]);
+//    }
+    
+}());
